4c5a30210cd35e0a276db23b32e215bfd138bfaf
Arbaz Ansari <mail.arbazansari@gmail.com>
2025-07-27 22:36:50 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.PRODUCTION_URL || "http://localhost:3000", 
+    origin: process.env.CLIENT_URL || process.env.PRODUCTION_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
